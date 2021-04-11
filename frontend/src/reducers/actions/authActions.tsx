46f911d239a849bdf125e18b2db0db37c7218888
david <dparrasmartinez@gmail.com>
2021-04-11 18:50:47 +0200

    redux authentication

--- frontend/src/reducers/actions/authActions.tsx
+++ frontend/src/reducers/actions/authActions.tsx
@@ -1,30 +1,36 @@
-import axios from 'axios'; 
-import {
-    REGISTER_SUCCESS,
-    REGISTER_FAIL
-} from './types';
+import axios from "axios";
+import { setAlert } from "./alertActions";
+import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
 
 // Register User
-export const register = ({ name, surname, email, password}) => async dispatch => {
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
+export const register = ({ name, surname, email, password }) => async (
+  dispatch
+) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const body = JSON.stringify({ name, surname, email, password });
+
+  try {
+    debugger;
+    const res = await axios.post("/api/users", body, config);
+
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data,
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "error")));
     }
 
-const body = JSON.stringify({ name, surname, email, password});
-
-try {
-    const res = await axios.post('/api/users', body, config);
-
     dispatch({
-        type: REGISTER_SUCCESS,
-        payload: res.data
-    })
-} catch(err){
-    dispatch({
-        type: REGISTER_FAIL
-    })
-}
-
-}
+      type: REGISTER_FAIL,
+    });
+  }
+};
