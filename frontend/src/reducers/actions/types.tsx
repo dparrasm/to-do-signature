92c13c6c566a8ad63c0cdc911f384f5f21d57e8b
david <dparrasmartinez@gmail.com>
2021-04-11 18:54:57 +0200

    commit

--- frontend/src/reducers/actions/types.tsx
+++ frontend/src/reducers/actions/types.tsx
@@ -2,5 +2,5 @@ export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
-export const LOG_IN = "LOG_IN";
-export const LOG_OUT = "LOG_OUT";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import alert from './alertReducer';
+import auth from './authReducer';
 //This object will contains all the reducers we create.
 export default combineReducers({
-    alert
+    alert,
+    auth
 });
